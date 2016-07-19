1701a4baf20ea74a2b713de80519eca8d6b06648
bagelnet <shumma_tanaka@dwango.co.jp>
2016-07-19 15:42:10 +0900

    apply multi image gif

--- js/gifMovie.js
+++ js/gifMovie.js
@@ -1,9 +1,12 @@
-function exportGifMovie(context) {
-    var encoder = new GIFEncoder();
-    encoder.setRepeat(0);
-    encoder.setDelay(500);
-    encoder.start();
-    encoder.addFrame(context); // ctx defined on camera.js
+encoder = new GIFEncoder();
+encoder.setRepeat(0);
+encoder.setDelay(500);
+encoder.start();
+
+function addPicture(context) {
+  encoder.addFrame(context); // context defined on camera.js
+}
+function exportGifMovie() {
     encoder.finish();
     var binary_gif = encoder.stream().getData() //notice this is different from the as3gif package!
     var data_url = 'data:image/gif;base64,'+encode64(binary_gif);
